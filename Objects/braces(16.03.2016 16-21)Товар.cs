8100c9862fd53553e9b48b0a93bcac77beb43557
Alexandr Sibiryakov <avsibiryakov@ics.perm.ru>
2016-03-16 16:43:01 +0500

    Init

---
+++ Objects/braces(16.03.2016 16-21)Товар.cs
@@ -0,0 +1,36 @@
+*** Start programmer edit section *** (Товар.Cтоимость CustomAttributes)
+
+*** End programmer edit section *** (Товар.Cтоимость CustomAttributes)
+
+*** Start programmer edit section *** (Товар.Cтоимость Get start)
+
+*** End programmer edit section *** (Товар.Cтоимость Get start)
+
+*** Start programmer edit section *** (Товар.Cтоимость Get end)
+
+*** End programmer edit section *** (Товар.Cтоимость Get end)
+
+*** Start programmer edit section *** (Товар.Cтоимость Set start)
+
+*** End programmer edit section *** (Товар.Cтоимость Set start)
+
+*** Start programmer edit section *** (Товар.Cтоимость Set end)
+
+*** End programmer edit section *** (Товар.Cтоимость Set end)
+
+*** Start programmer edit section *** (Товар.ПоступлениеТовара CustomAttributes)
+
+*** End programmer edit section *** (Товар.ПоступлениеТовара CustomAttributes)
+
+*** Start programmer edit section *** (Товар.ПоступлениеТовара method implementation)
+            return;
+*** End programmer edit section *** (Товар.ПоступлениеТовара method implementation)
+
+*** Start programmer edit section *** (Товар.ПродажаТовара CustomAttributes)
+
+*** End programmer edit section *** (Товар.ПродажаТовара CustomAttributes)
+
+*** Start programmer edit section *** (Товар.ПродажаТовара method implementation)
+            return;
+*** End programmer edit section *** (Товар.ПродажаТовара method implementation)
+
